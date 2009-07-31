16ecfd1404decfa327d5f994d8bc6972ccc3079b
Timothy Baldock <tb@entropy.me.uk>
2009-07-31 16:54:09 +0000

    Add cubic interpolate function
    git-svn-id: svn+ssh://entropy.me.uk/var/svn/timothy/pytile/trunk@215 741c516a-3ba6-4fa8-b112-5c51f26a88df

--- random_test.py
+++ random_test.py
@@ -5,21 +5,37 @@ import sys, os, random
 
 pygame.init()
 
-X_SCREEN = 200
-Y_SCREEN = 200
+X_SCREEN = 100
+Y_SCREEN = 100
 
 
 screen = pygame.display.set_mode([X_SCREEN, Y_SCREEN])
 surface = pygame.Surface([X_SCREEN, Y_SCREEN])
 
 
-persistence = 0.8
+persistence = 0.2
 num_octaves = 8
 
 randoms = []
 
-def Interpolate(a, b, x):
+def LinearInterpolate(a, b, x):
     return a*(1-x) + b*x
+
+##  function Cubic_Interpolate(v0, v1, v2, v3,x)
+##	P = (v3 - v2) - (v0 - v1)
+##	Q = (v0 - v1) - P
+##	R = v2 - v0
+##	S = v1
+##
+##	return Px3 + Qx2 + Rx + S
+##  end of function
+
+def CubicInterpolate(v0, v1, v2, v3, x):
+    P = (v3 - v2) - (v0 - v1)
+    Q = (v0 - v1) - P
+    R = v2 - v0
+    S = v1
+    return P*math.pow(x,3) + Q*math.pow(x,2) + R*x + S
 
 def Noise(i, x, y):
 ##    random.seed(i)
@@ -43,10 +59,11 @@ def InterpolatedNoise(i, x, y):
     v3 = SmoothedNoise(i, x_int, y_int+1)
     v4 = SmoothedNoise(i, x_int+1, y_int+1)
 
-    i1 = Interpolate(v1, v2, x_frac)
-    i2 = Interpolate(v3, v4, x_frac)
+##    i1 = LinearInterpolate(v1, v2, x_frac)
+##    i2 = LinearInterpolate(v3, v4, x_frac)
+##    return LinearInterpolate(i1, i2, y_frac)
 
-    return Interpolate(i1, i2, y_frac)
+    return CubicInterpolate(v1, v2, v3, v4, x_frac)
 
 def PerlinNoise_2D(x, y):
 ##    print x, y
