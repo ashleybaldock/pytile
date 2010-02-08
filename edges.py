1f6c182ab9c982d78b71905d1ae9b5c621c291b4
Timothy Baldock <tb@entropy.me.uk>
2010-02-08 23:32:05 +0000

    Work on the midpoint line generation for left-right drawing
    git-svn-id: svn+ssh://entropy.me.uk/var/svn/timothy/pytile/trunk@396 741c516a-3ba6-4fa8-b112-5c51f26a88df

--- edges.py
+++ edges.py
@@ -64,14 +64,19 @@ class TileDraw(object):
         self.start_2A = [self.up_x, self.up_3x4, self.up_x2, self.up_x4, self.up_0]
         self.start_2B = [self.down_0, self.down_x4, self.down_x2, self.down_3x4, self.down_x]
         self.start_3 = [self.down_x, self.down_3x4, self.down_x2, self.down_x4, self.down_0]
+        self.start_C = [self.up_x2, self.up_x3, self.up_x4, self.up_x8, self.up_0, self.down_x8, self.down_x4, self.down_x3, self.down_x2]
     def up_x(self, x):
         return x
     def up_3x4(self, x):
         return int(math.ceil(x/2.0) + math.floor(x/4.0))
     def up_x2(self, x):
         return x / 2
+    def up_x3(self, x):
+        return x / 3
     def up_x4(self, x):
         return x / 4
+    def up_x8(self, x):
+        return x / 8
     def up_0(self, x):
         return 0
     def down_x(self, x):
@@ -80,8 +85,12 @@ class TileDraw(object):
         return - self.up_3x4(x)
     def down_x2(self, x):
         return - self.up_x2(x)
+    def down_x3(self, x):
+        return - self.up_x3(x)
     def down_x4(self, x):
         return - self.up_x4(x)
+    def down_x8(self, x):
+        return - self.up_x8(x)
     def down_0(self, x):
         return - self.up_0(x)
     def draw_tile(self, p, pix, offx, offy, tile):
@@ -93,6 +102,9 @@ class TileDraw(object):
         # Determine which way around the tile is to be drawn
         # Either left+right to middle, or top+bottom to sides
         # Then, call draw_segment for the two halves of the tile
+        self.draw_segment(1, tile[1], tile[0], tile[2])
+        self.draw_segment(3, tile[3], tile[0], tile[2])
+        return
         if tile[0] == tile[2]:
             self.draw_segment(1, tile[1], tile[0], tile[2])
             self.draw_segment(3, tile[3], tile[0], tile[2])
@@ -113,17 +125,21 @@ class TileDraw(object):
                 for y in range(self.offy + 48 - start_height * self.p/8 - self.start_0A[gradient_A](x), 
                                self.offy + 49 - start_height * self.p/8 - self.start_0B[gradient_B](x)):
                     self.pix[self.offx + x, y] = (200,0,0)
+
         elif start_corner == 1:
             # Left corner to top corner, bottom constrained to left-right midline
+            gradient_C = end_height_A - end_height_B + 4
+            gradient_D = end_height_B - end_height_A + 4
             for x in range(0, 32):
                 for y in range(self.offy + 48 - end_height_A * self.p/8 - self.start_1[gradient_A](x), 
-                               self.offy + 49 - end_height_A * self.p/8):
+                               self.offy + 49 - end_height_A * self.p/8 - self.start_C[gradient_C](x)):
                     self.pix[self.offx + x, y] = (100,0,0)
             # Right corner to top corner, bottom constrained to left-right midline
             for x in range(32, 64):
                 for y in range(self.offy + 48 - end_height_B * self.p/8 - self.start_1[gradient_B](63 - x), 
-                               self.offy + 49 - end_height_B * self.p/8):
+                               self.offy + 49 - end_height_B * self.p/8 - self.start_C[gradient_D](63 - x)):
                     self.pix[self.offx + x, y] = (100,0,0)
+
         elif start_corner == 2:
             # Starting at left, so end_height_A is top, end_height_B is bottom
             # Since we're starting at A, range is 32
@@ -132,14 +148,17 @@ class TileDraw(object):
                                self.offy + 49 - start_height * self.p/8 - self.start_0B[gradient_B](63 - x)):
                     self.pix[self.offx + x, y] = (0,0,200)
         else:
+            return
             # Left corner to bottom corner, top constrained to left-right midline
+            gradient_C = end_height_A - end_height_B + 4
+            gradient_D = end_height_B - end_height_A + 4
             for x in range(0, 32):
-                for y in range(self.offy + 48 - end_height_A * self.p/8, 
+                for y in range(self.offy + 48 - end_height_A * self.p/8 - self.start_C[gradient_C](x),
                                self.offy + 49 - end_height_A * self.p/8 - self.start_3[gradient_A](x)):
                     self.pix[self.offx + x, y] = (100,100,0)
             # Right corner to bottom corner, top constrained to left-right midline
             for x in range(32, 64):
-                for y in range(self.offy + 48 - end_height_B * self.p/8, 
+                for y in range(self.offy + 48 - end_height_B * self.p/8 - self.start_C[gradient_D](63 - x), 
                                self.offy + 49 - end_height_B * self.p/8 - self.start_3[gradient_B](63 - x)):
                     self.pix[self.offx + x, y] = (100,100,0)
 
